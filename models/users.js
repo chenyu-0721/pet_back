b389399b41eeb8eed81e2ad7cdf86e6441df1501
陳冠宇 <chen630602@gmail.com>
2024-06-19 22:17:28 +0800

    登入註冊

---
+++ models/users.js
@@ -0,0 +1,26 @@
+// models/users.js
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "請輸入您的 Email"],
+    unique: true,
+    lowercase: true,
+    select: false,
+  },
+  password: {
+    type: String,
+    required: [true, "請輸入密碼"],
+    minlength: 8,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
